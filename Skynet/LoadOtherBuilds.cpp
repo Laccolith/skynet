388362d1ec0dbfdc0c9f225b9d62263170e1d128
Andrew Smith <Quaver.smith@gmail.com>
2015-10-06 22:18:11 +0100

    Cleaning House

--- Skynet/LoadOtherBuilds.cpp
+++
@@ -1,9 +0,0 @@
-#include "BuildOrderManager.h"
-
-#include "PlayerTracker.h"
-
-void BuildOrderManagerClass::LoadOtherBuilds()
-{
-	mBuildOrders[BuildOrderID::Finished] = BuildOrder(BuildOrderID::Finished, "Finished");
-	mBuildOrders[BuildOrderID::None] = BuildOrder(BuildOrderID::None, "None");
-}
--- Skynet/LoadZergBuilds.cpp
+++
@@ -1,8 +0,0 @@
-#include "BuildOrderManager.h"
-
-void BuildOrderManagerClass::LoadZergBuilds()
-{
-	int ID_1 = 0;
-	int ID_2 = 0;
-	int ID_3 = 0;
-}
